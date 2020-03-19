5dd6cf51eeb19264c0e59a1d76b3689015fead89
Tom <trwole13@gmail.com>
2020-03-19 17:25:40 +0000

    Add dwarf names and more subraces

--- src/generator/names/index.ts
+++ src/generator/names/index.ts
@@ -2,6 +2,7 @@
 import * as AarakocraNames from './aarakocra.json';
 import * as AasimarNames from './aasimar.json';
 import * as DragonbornNames from './dragonborn.json';
+import * as DwarfNames from './dwarf.json';
 import * as ElfNames from './elf.json';
 import * as AirGenasiNames from './genasi-air.json';
 import * as EarthGenasiNames from './genasi-earth.json';
@@ -24,13 +25,24 @@ import * as YuanTiNames from './yuan-ti.json';
 export const NameDefinitions: { [key: string]: any[] } = {
   'Aarakocra': [AarakocraNames],
   'Aasimar': [AasimarNames],
+  'Aquatic Half-Elf': [ElfNames],
+  'Scourge Aasimar': [AasimarNames],
+  'Protector Aasimar': [AasimarNames],
+  'Fallen Aasimar': [AasimarNames],
   'Air Genasi': [AirGenasiNames],
   'Bugbear': [GoblinNames],
+  'Deep Gnome': [GnomeNames],
   'Dragonborn': [DragonbornNames],
+  'Drow Half-Elf': [ElfNames],
+  'Duergar': [DwarfNames],
+  'Dwarf': [DwarfNames],
+  'Eladrin Elf': [ElfNames],
   'Elf': [ElfNames],
   'Earth Genasi': [EarthGenasiNames],
+  'Feral Tiefling': [TieflingNames],
   'Firbolg': [ElfNames],
   'Fire Genasi': [FireGenasiNames],
+  'Forest Gnome': [GnomeNames],
   'Genasi': [AirGenasiNames, EarthGenasiNames, FireGenasiNames, WaterGenasiNames],
   'Gnome': [GnomeNames],
   'Goblin': [GoblinNames],
@@ -38,15 +50,24 @@ export const NameDefinitions: { [key: string]: any[] } = {
   'Half-Elf': [ElfNames, HumanNames],
   'Half-Orc': [OrcNames, HumanNames],
   'Halfling': [HalflingNames],
+  'Hill Dwarf': [DwarfNames],
+  'High Elf': [ElfNames],
+  'High Half-Elf': [ElfNames],
   'Hobgoblin': [GoblinNames],
   'Human': [HumanNames],
   'Kenku': [KenkuNames],
   'Kobold': [KoboldNames],
+  'Light Halfling': [HalflingNames],
   'Lizardfolk': [LizardfolkNames],
+  'Mountain Dwarf': [DwarfNames],
   'Orc': [OrcNames],
+  'Rock Gnome': [GnomeNames],
+  'Stout Halfling': [HalflingNames],
   'Tabaxi': [TabaxiNames],
   'Tiefling': [TieflingNames],
   'Triton': [TritonNames],
   'Water Genasi': [WaterGenasiNames],
+  'Wood Elf': [ElfNames],
+  'Wood Half-Elf': [ElfNames],
   'Yuan-ti Pureblood': [YuanTiNames]
 };
